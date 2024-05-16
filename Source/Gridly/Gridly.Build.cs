e6e522fcca724cdae0d91952ff838ac79087c147
norbertszolnoki <ns@localizedirect.com>
2024-05-16 18:58:36 +0200

    Initial_commit

---
+++ Source/Gridly/Gridly.Build.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) 2021 LocalizeDirect AB
+
+using UnrealBuildTool;
+
+public class Gridly : ModuleRules
+{
+	public Gridly(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+            "Json",
+            "HTTP"
+        });
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{							
+				"InputCore",
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",                
+                "Serialization",				
+				"JsonUtilities"
+			}
+			);
+
+		if (Target.bBuildEditor)
+        {
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "Settings",
+                    "Localization"
+                }
+                );
+        }
+	}
+}
---
+++ Source/Gridly/Private/Gridly.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Modules/ModuleInterface.h"
+#include "Engine.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogGridly, Log, Log);
+
+class FGridlyModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+};
